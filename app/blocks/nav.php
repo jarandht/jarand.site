0f8d7f764a622a7a776db81f425d1d659f9d8d2e
jarandht <ost515132@gmail.com>
2024-05-28 23:11:32 +0200

    Gimme Sympathy

---
+++ app/blocks/nav.php
@@ -0,0 +1,25 @@
+<link rel="stylesheet" href="/css/nav.css">
+
+<nav class="nav">
+    <div class="profile">
+        <div class="profileImg" style="background: url(./img/egg5.jpg); background-position: top; background-size: cover; " alt=""></div>
+        <h1>Jarand Holmefjord Tyssen</h1>
+    </div>
+    <div class="navButtons">
+        <a href="/"><div class="navButtonActive"><img src="/img/homegrey.png" alt=""><p>Home</p></div></a>
+        <div class="navButtonDropdown">
+            <img src="/img/gray_docs.png" alt=""><p>Projects</p>
+            <i></i>
+            <a href=""><span class="navButtonDropdown_child">
+                <img src="/img/gray_internet.png" alt=""><p>Websites</p>
+            </span></a>
+            <a href=""><span class="navButtonDropdown_child">
+                <img src="/img/gray_cloud_server.png" alt=""><p>Homelab</p>
+            </span></a>
+        </div>
+        <a href="/personal_sites"><div><img src="/img/gray_edit.png" alt=""><p>Personal Sites</p></div></a>
+    </div>
+    <div class="copyright">
+        <p>© - jarand.site - 2024</p>
+    </div>
+</nav>
