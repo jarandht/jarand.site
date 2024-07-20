37a581e0eca523454f1304341b68beeead7e8047
jarandht <ost515132@gmail.com>
2024-07-20 22:12:35 +0200

    logo downgrade

--- app/blocks/head.php
+++ app/blocks/head.php
@@ -2,5 +2,4 @@
     <link rel="stylesheet" href="/css/all.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" type="image/png" href="/img/egg5.jpg"/>
 </head>
